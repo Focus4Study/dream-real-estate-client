6fc3cea11680a8c6ee1faebcf7c00e6583a829b4
Focus4Study <f4studies@gmail.com>
2024-06-14 10:30:27 +0600

    advertisement section fixed

--- src/Pages/Home/AdvertisementSection/AdvertisementSection.jsx
+++ src/Pages/Home/AdvertisementSection/AdvertisementSection.jsx
@@ -17,25 +17,27 @@ const AdvertisementSection = () => {
             <p className='lg:w-3/5 px-2 text-center mx-auto py-5 lg:py-10'>We offer a diverse selection of properties to suit your lifestyle, from cozy condos in the heart of the city to spacious family homes in quiet suburbs.  Enjoy breathtaking views, top-rated schools, and convenient access to shopping, dining, and entertainment. Our experienced team is dedicated to helping you find the perfect place to call home.
             </p>
 
+            <div className='md:hidden'>
 
-            {
-                adProperties.map(property => <div key={property._id}>
-                    <div className="lg:max-w-sm m-5 rounded-md bg-white shadow-md dark:bg-gray-50 dark:text-gray-800 mx-auto">
-                        <img src={property.property_image} alt="" className="object-cover object-center w-full rounded-t-md h-72 dark:bg-gray-500" />
-                        <div className="flex flex-col justify-between p-6 space-y-8">
-                            <div className="space-y-2">
-                                <h2 className="text-2xl h-16 font-semibold tracking-wide">{property.property_title}</h2>
-                                <p className="dark:text-gray-800">{property.property_location}</p>
-                                <p className="dark:text-gray-800">{property.verification_status}</p>
-                                <p className="dark:text-gray-800">{property.price_range}</p>
+                {
+                    adProperties.map(property => <div key={property._id}>
+                        <div className="lg:max-w-sm m-5 rounded-md bg-white shadow-md dark:bg-gray-50 dark:text-gray-800 mx-auto">
+                            <img src={property.property_image} alt="" className="object-cover object-center w-full rounded-t-md h-72 dark:bg-gray-500" />
+                            <div className="flex flex-col justify-between p-6 space-y-8">
+                                <div className="space-y-2">
+                                    <h2 className="text-2xl h-16 font-semibold tracking-wide">{property.property_title}</h2>
+                                    <p className="dark:text-gray-800">{property.property_location}</p>
+                                    <p className="dark:text-gray-800">{property.verification_status}</p>
+                                    <p className="dark:text-gray-800">{property.price_range}</p>
+                                </div>
+                                <Link to={`/property/details/${property._id}`}>
+                                    <button type="button" className="flex btn-ghost bg-emerald-400 items-center justify-center w-full p-3 font-semibold tracking-wide rounded-md dark:bg-violet-600 dark:text-gray-50">Details button</button>
+                                </Link>
                             </div>
-                            <Link to={`/property/details/${property._id}`}>
-                                <button type="button" className="flex btn-ghost bg-emerald-400 items-center justify-center w-full p-3 font-semibold tracking-wide rounded-md dark:bg-violet-600 dark:text-gray-50">Details button</button>
-                            </Link>
                         </div>
-                    </div>
-                </div>)
-            }
+                    </div>)
+                }
+            </div>
 
             <div className='hidden sm:block'>
                 <Swiper
